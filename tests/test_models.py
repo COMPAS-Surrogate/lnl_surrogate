489470f343cd4189c17d788715bd273bd47092b3
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2024-01-09 13:28:56 +0530

    add parameterized models

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,24 +1,26 @@
 import tensorflow as tf
-from trieste.acquisition.function import PredictiveVariance
+from trieste.acquisition.function import PredictiveVariance, ExpectedImprovement
 
 import numpy as np
 from lnl_surrogate.surrogate import train, load
+import pytest
 
 
-def test_1d(mock_data):
+@pytest.mark.parametrize('model_type', ['gp', 'deepgp'])
+def test_1d(mock_data, tmpdir, model_type):
+    outdir = f'{tmpdir}/model_type'
     res = train(
-        model_type='gp',
+        model_type=model_type,
         mcz_obs=mock_data.observations.mcz,
         compas_h5_filename=mock_data.compas_filename,
-        acquisition_fns=[PredictiveVariance()],
+        acquisition_fns=[ExpectedImprovement()],
         params=['aSF'],
         n_init=2,
         n_rounds=1,
         n_pts_per_round=1,
-        outdir='out_test',
+        outdir=outdir,
     )
     assert res is not None
-    model = load('out_test')
+    model = load(outdir)
     lnl, _ = model.predict(np.array([[0.1]]))
     assert isinstance(tf.squeeze(lnl).numpy(), float)
-
